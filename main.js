54416ed0d09282399480994e9c9e6cf858e8dd9e
hajdur <hajdu.reka1109@gmail.com>
2023-10-11 15:56:17 +0200

    playfield starter

--- main.js
+++ main.js
@@ -1,65 +1,97 @@
+function startOnLoad() {
+	displayName();
+	createSmall();
+}
 // function displayName() {
 // 	var playerName = prompt("What's your name?");
 // 	if (playerName != null) {
 // 		document.getElementById("greeting").innerText = "Hello " + playerName + "!";
 // 	}
 // }
-
-var grid = document.getElementById("grid");
-
 function createSmall() {
-	document.querySelector(".playfield").style.gridColumn = "10";
+	var playfield = document.querySelector(".playfield");
+
+	playfield.classList.add("playfield-small");
+	playfield.classList.remove("playfield-medium");
+	playfield.classList.remove("playfield-large");
+
+	playfield.classList.remove("playfield-default");
+
+	$(".playfield").empty();
+
+	for (let i = 0; i < 49; i++) {
+		var field = document.createElement("div");
+		field.classList.add("field");
+		field.setAttribute("data-x", i);
+		playfield.append(field);
+		var fieldCover = document.createElement("div");
+		fieldCover.classList.add("fieldCover");
+		field.append(fieldCover);
+
+		var fieldCount = i;
+	}
+
+	for (let i = 0; i < 5; i++) {
+		var bombField = Math.random();
+		bombField = bombField * fieldCount;
+		bombField = Math.floor(bombField);
+		let field = $("[data-x=" + bombField + "]");
+		console.log(bombField);
+		field.text("💣");
+	}
+
+	for (let i = 0; i < 5; i++) {
+		if (field.getAttribute("data-x") === bombField) {
+			field.innerText = "💣";
+		}
+	}
 }
-function chooseSmall() {
-	var field = document.querySelector(".playfield");
-
-	field.classList.add("playfield-small");
-	field.classList.remove("playfield-medium");
-	field.classList.remove("playfield-large");
-
-	field.classList.remove("playfield-default");
-
-	createSmall();
-}
-
 function createMedium() {
-	document.querySelector(".playfield").style.gridColumn = "14";
+	var playfield = document.querySelector(".playfield");
+
+	playfield.classList.remove("playfield-small");
+	playfield.classList.add("playfield-medium");
+	playfield.classList.remove("playfield-large");
+
+	playfield.classList.remove("playfield-default");
+
+	$(".playfield").empty();
+
+	for (let i = 0; i < 100; i++) {
+		var field = document.createElement("div");
+		field.classList.add("field");
+		field.setAttribute("data-x", i);
+		playfield.append(field);
+		var fieldCover = document.createElement("div");
+		fieldCover.classList.add("fieldCover");
+		field.append(fieldCover);
+	}
 }
-
-function chooseMedium() {
-	var field = document.querySelector(".playfield");
-
-	field.classList.remove("playfield-small");
-	field.classList.add("playfield-medium");
-	field.classList.remove("playfield-large");
-
-	field.classList.remove("playfield-default");
-
-	createMedium();
-}
-
 function createLarge() {
-	document.querySelector(".playfield").style.gridColumn = "18";
+	var playfield = document.querySelector(".playfield");
+
+	playfield.classList.remove("playfield-small");
+	playfield.classList.remove("playfield-medium");
+	playfield.classList.add("playfield-large");
+
+	playfield.classList.remove("playfield-default");
+
+	$(".playfield").empty();
+
+	for (let i = 0; i < 196; i++) {
+		var field = document.createElement("div");
+		field.classList.add("field");
+		field.setAttribute("data-x", i);
+		playfield.append(field);
+		var fieldCover = document.createElement("div");
+		fieldCover.classList.add("fieldCover");
+		field.append(fieldCover);
+	}
 }
-
-function chooseLarge() {
-	var field = document.querySelector(".playfield");
-
-	field.classList.remove("playfield-small");
-	field.classList.remove("playfield-medium");
-	field.classList.add("playfield-large");
-
-	field.classList.remove("playfield-default");
-
-	createLarge();
-}
-
 function startOver() {
 	var field = document.querySelector(".playfield");
 
-	field.classList.add("playfield-default");
-
-	field.classList.remove("playfield-small");
+	field.classList.add("playfield-small");
 	field.classList.remove("playfield-medium");
 	field.classList.remove("playfield-large");
 }
@@ -79,9 +111,3 @@ function youWin() {
 	document.getElementById("gameStatus").innerText = "You won!";
 	document.getElementById("gameStatus").style.color = "green";
 }
-
-// if (
-// 	document.querySelector(".playfield").classList.contains("playfield-default")
-// ) {
-// 	document.getElementById("lost").style.display = none;
-// }
