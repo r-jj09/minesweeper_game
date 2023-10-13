40246f0659dc14a7367c85ec184ea86ebaf6d1a4
hajdur <hajdu.reka1109@gmail.com>
2023-10-13 10:29:29 +0200

    somewhat working

--- main.js
+++ main.js
@@ -1,132 +1,102 @@
-// function displayName() {
-// 	var playerName = prompt("What's your name?");
-// 	if (playerName != null) {
-// 		document.getElementById("greeting").innerText = "Hello " + playerName + "!";
-// 	}
-// }
-
-// var easy = document.getElementById("small-btn");
-// var medium = document.getElementById("medium-btn");
-// var hard = document.getElementById("large-btn");
-
-// if (easy.addEventListener("click")) {
-// 	createGame();
-// } else if (medium.addEventListener("click")) {
-// 	createGame();
-// } else if (hard.addEventListener("click")) {
-// 	createGame();
-// }
-
-// function createGame() {}
-
-function createSmall() {
-	var playfield = document.querySelector(".playfield");
-	playfield.classList.add("playfield-small");
-	playfield.classList.remove("playfield-medium");
-	playfield.classList.remove("playfield-large");
-
-	playfield.classList.remove("playfield-default");
-
-	$(".playfield").empty();
-
-	var num = 1;
-	for (let i = 0; i < 7; i++) {
-		for (let j = 0; j < 7; j++) {
-			var field = document.createElement("div");
-			field.classList.add("field");
-			field.setAttribute("data-x", i);
-			field.setAttribute("data-y", j);
-			field.setAttribute("data-num", num);
-			playfield.append(field);
-			var fieldCount = num;
-			num++;
-		}
+function displayName() {
+	var playerName = prompt("What's your name?");
+	if (playerName != null) {
+		document.getElementById("greeting").innerText = "Hello " + playerName + "!";
 	}
-	// console.log(fieldCount);
-	var bombCount = 4;
-	do {
-		var bombIndex = Math.random();
-		bombIndex = bombIndex * fieldCount;
-		bombIndex = Math.floor(bombIndex);
-		let field = $("[data-num=" + bombIndex + "]");
-		if (field.attr("data-bomb") !== "1") {
-			field.attr("data-bomb", 1);
-			--bombCount;
+}
+var won = true;
+var bombCount;
+var size;
+$(document).ready(function () {
+	var playfield = document.querySelector(".playfield");
+	var difficulty = document.querySelector(".difficulty");
+	$(".buttons").on("click", ".size-btn", (event) => {
+		difficulty.style.visiblity = "hidden";
+		if (event.currentTarget.classList.contains("small-btn")) {
+			bombCount = 5;
+			size = 7;
+			playfield.classList.add("playfield-small");
+			playfield.classList.remove("playfield-medium");
+			playfield.classList.remove("playfield-large");
+		} else if (event.currentTarget.classList.contains("medium-btn")) {
+			bombCount = 8;
+			size = 10;
+			playfield.classList.remove("playfield-small");
+			playfield.classList.add("playfield-medium");
+			playfield.classList.remove("playfield-large");
+		} else if (event.currentTarget.classList.contains("large-btn")) {
+			bombCount = 12;
+			size = 14;
+			playfield.classList.remove("playfield-small");
+			playfield.classList.remove("playfield-medium");
+			playfield.classList.add("playfield-large");
 		}
-		console.log(bombIndex);
-	} while (bombCount >= 0);
-
-	$(".playfield").on("click", ".field", (event) => {
-		if (event.currentTarget.getAttribute("data-bomb") == 1) {
-			event.currentTarget.style.backgroundColor = "rgba(0, 0, 0, 0.342)";
-			event.currentTarget.innerText = "💣";
-		} else {
-			event.currentTarget.style.backgroundColor = "white";
-			walkAround();
-		}
+		createGame();
 	});
-}
+	function createGame() {
+		$(".playfield").empty();
+		var num = 1;
+		for (let i = 0; i < size; i++) {
+			for (let j = 0; j < size; j++) {
+				var field = document.createElement("div");
+				field.classList.add("field");
+				field.setAttribute("data-x", i);
+				field.setAttribute("data-y", j);
+				field.setAttribute("data-num", num);
+				playfield.append(field);
+				var fieldCount = num;
+				num++;
+			}
+		}
+		console.log(fieldCount);
+		do {
+			var bombIndex = Math.random();
+			bombIndex = bombIndex * fieldCount;
+			bombIndex = Math.floor(bombIndex);
+			let field = $("[data-num=" + bombIndex + "]");
+			if (field.attr("data-bomb") !== "1") {
+				field.attr("data-bomb", 1);
+				--bombCount;
+			}
+			console.log(bombIndex);
+		} while (bombCount >= 0);
 
-function walkAround() {
-	return;
-}
+		$(".playfield").on("click", ".field", (event) => {
+			if (event.currentTarget.getAttribute("data-bomb") == 1) {
+				event.currentTarget.style.backgroundColor = "red";
+				event.currentTarget.innerText = "💣";
+				won = false;
+				gameOver();
+			} else {
+				event.currentTarget.style.backgroundColor = "rgba(0, 0, 0, 0.342)";
+				walkAround();
+			}
+		});
 
-function createMedium() {
-	var playfield = document.querySelector(".playfield");
-
-	playfield.classList.remove("playfield-small");
-	playfield.classList.add("playfield-medium");
-	playfield.classList.remove("playfield-large");
-
-	playfield.classList.remove("playfield-default");
-
-	$(".playfield").empty();
-
-	for (let i = 0; i < 100; i++) {
-		var field = document.createElement("div");
-		field.classList.add("field");
-		field.setAttribute("data-x", i);
-		playfield.append(field);
-		var fieldCover = document.createElement("div");
-		fieldCover.classList.add("fieldCover");
-		field.append(fieldCover);
+		function walkAround() {
+			for (let i = 0; i < array.length; i++) {}
+			return;
+		}
 	}
-}
-function createLarge() {
-	var playfield = document.querySelector(".playfield");
+});
 
-	playfield.classList.remove("playfield-small");
-	playfield.classList.remove("playfield-medium");
-	playfield.classList.add("playfield-large");
-
-	playfield.classList.remove("playfield-default");
-
-	$(".playfield").empty();
-
-	for (let i = 0; i < 196; i++) {
-		var field = document.createElement("div");
-		field.classList.add("field");
-		field.setAttribute("data-x", i);
-		playfield.append(field);
-		var fieldCover = document.createElement("div");
-		fieldCover.classList.add("fieldCover");
-		field.append(fieldCover);
-	}
-}
-function startOver() {
-	var field = document.querySelector(".playfield");
-
-	field.classList.add("playfield-small");
-	field.classList.remove("playfield-medium");
-	field.classList.remove("playfield-large");
-}
 function gameOver() {
 	document.getElementById("status").style.display = "flex";
-	if (condition) {
+	if (won === false) {
 		document.getElementById("gameStatus").innerText = "You lost!";
 		document.getElementById("gameStatus").style.color = "red";
 	} else {
 		document.getElementById("gameStatus").innerText = "You won!";
 		document.getElementById("gameStatus").style.color = "green";
 	}
+}
+function startOver() {
+	document.getElementById("status").style.display = "none";
+	$(".playfield").empty();
+
+	var field = document.querySelector(".playfield");
+
+	field.classList.add("playfield-small");
+	field.classList.remove("playfield-medium");
+	field.classList.remove("playfield-large");
 }
